6776cd0acf2b1094d0d983f230014959ac237eb5
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2022-10-12 14:11:18 -0500

    Readd __init__.py files

---
+++ mosdef_gomc/__init__.py
@@ -0,0 +1,5 @@
+"""mBuild: a hierarchical, component based molecule builder."""
+from mosdef_gomc.formats.charmm_writer import Charmm
+from mosdef_gomc.formats.gomc_conf_writer import GOMCControl
+
+__version__ = "0.1.0"
