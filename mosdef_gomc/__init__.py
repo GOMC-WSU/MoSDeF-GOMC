6c6965693c6d8a86931f8d22d7076499b29fcc1b
bc118 <crawfordb118@gmail.com>
2022-10-20 14:37:30 -0400

    changed init file

--- mosdef_gomc/__init__.py
+++ mosdef_gomc/__init__.py
@@ -1,5 +1,7 @@
-"""mBuild: a hierarchical, component based molecule builder."""
-from mosdef_gomc.formats.charmm_writer import Charmm
-from mosdef_gomc.formats.gomc_conf_writer import GOMCControl
+"""MoSDeF-GOMC: Python software for the creation of scientific workflows for the Monte Carlo simulation engine GOMC"""
+from mosdef_gomc.formats.gmso_charmm_writer import Charmm
+from mosdef_gomc.formats.gmso_gomc_conf_writer import GOMCControl
+from mosdef_gomc.formats.charmm_writer import parmed_Charmm
+from mosdef_gomc.formats.gomc_conf_writer import parmed_GOMCControl
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
