ae1ea868123a20ce2f72e38e5bd58f5e0a4e155f
bc118 <crawfordb118@gmail.com>
2025-08-02 11:22:40 -0400

    Bump to version 1.5.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 #########################################
 NAME = "MoSDeF-GOMC"
-VERSION = "1.4.0"
+VERSION = "1.5.0"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
